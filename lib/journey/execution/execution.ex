0bb1db884d09f0261b0bbfe1ae06991786b03d8e
Mark Markaryan <markmark206@gmail.com>
2022-10-22 16:15:03 -0700

    tweaking execution summary texts

--- lib/journey/execution/execution.ex
+++ lib/journey/execution/execution.ex
@@ -28,22 +28,22 @@ defmodule Journey.Execution do
     execution |> Journey.Execution.Store.load()
   end
 
-  defp computation_summary(computation) do
-    conditional_timestamp = fn epoch_seconds ->
-      if epoch_seconds != nil and epoch_seconds > 0 do
-        "#{Journey.Utilities.epoch_to_timestamp(epoch_seconds)} (#{epoch_seconds})"
-      else
-        0
-      end
+  defp conditional_timestamp(epoch_seconds) do
+    if epoch_seconds != nil and epoch_seconds > 0 do
+      "#{Journey.Utilities.epoch_to_timestamp(epoch_seconds)} (#{epoch_seconds})"
+    else
+      0
     end
+  end
 
+  defp computation_summary(computation) do
     """
-    Computation #{computation.ex_revision}: '#{computation.id}' / '#{computation.name}:'
+    Revision #{computation.ex_revision}: '#{computation.id}' / '#{computation.name}:'
       Result: '#{computation.result_code}'
-      Started at: #{conditional_timestamp.(computation.start_time)}
-      Ended at: #{conditional_timestamp.(computation.end_time)}
-      Deadline: #{conditional_timestamp.(computation.deadline)}).
-      Scheduled for: #{conditional_timestamp.(computation.scheduled_time)}
+      Started at: #{conditional_timestamp(computation.start_time)}
+      Ended at: #{conditional_timestamp(computation.end_time)}
+      Deadline: #{conditional_timestamp(computation.deadline)}).
+      Scheduled for: #{conditional_timestamp(computation.scheduled_time)}
     """
   end
 
@@ -54,9 +54,13 @@ defmodule Journey.Execution do
 
   def get_summary(execution) do
     """
-    ** Execution Summary **
-    id: #{execution.id}
-    computations:\n#{execution.computations |> computations_summary() |> Enum.map_join("\n", fn c -> "* #{c}" end)}
+    # Execution Summary
+    ID: #{execution.id}
+    Process: #{execution.process_id}
+    Revisions: #{execution.revision}
+    Created at: #{conditional_timestamp(execution.inserted_at)}
+    Last updated at: #{conditional_timestamp(execution.updated_at)}
+    Computations:\n#{execution.computations |> computations_summary() |> Enum.map_join("\n", fn c -> "* #{c}" end)}
     """
   end
 end
